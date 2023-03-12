0406a4f6ad34fe98ba4885a9f394d5440acb8fd3
ChinenOFF <pro100cvv@gmail.com>
2023-03-12 10:37:30 +0300

    task #1 Sequences completed  - package sequences; task #2 Complex Numbers completed  - package complexNumbers;

---
+++ src/main/java/org/example/homework2/complexNumbers/ComplexNumbers.java
@@ -0,0 +1,37 @@
+package org.example.homework2.complexNumbers;
+
+public class ComplexNumbers {
+    private final double real;
+    private final double imaginary;
+
+    public ComplexNumbers(double r) {
+        real = r;
+        imaginary = 0;
+    }
+
+    public ComplexNumbers(double r, double im) {
+        real = r;
+        imaginary = im;
+    }
+
+    public ComplexNumbers sum(ComplexNumbers b) {
+        return new ComplexNumbers(this.real + b.real, this.imaginary + b.imaginary);
+    }
+
+    public ComplexNumbers subtract(ComplexNumbers b) {
+        return new ComplexNumbers(this.real - b.real, this.imaginary - b.imaginary);
+    }
+
+    public ComplexNumbers multiply(ComplexNumbers b) {
+        return new ComplexNumbers(this.real * b.real - this.imaginary * b.imaginary, this.real * b.imaginary + this.imaginary * b.real);
+    }
+
+    public double abs() {
+        return Math.hypot(this.real, this.imaginary);
+    }
+
+    @Override
+    public String toString() {
+        return "" + real + ((imaginary < 0) ? ("-i" + (-imaginary)) : ("+i" + imaginary));
+    }
+}
