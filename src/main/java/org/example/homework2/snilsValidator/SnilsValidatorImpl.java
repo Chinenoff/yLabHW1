159525c3a49b12275ff5ea55e3dd4348417fd871
ChinenOFF <pro100cvv@gmail.com>
2023-03-13 11:48:47 +0300

    task #5 StatsAccumulator completed  - package statsAccumulator; added checking the correctness of entering a string into the task #4 SnilsValidator;

--- src/main/java/org/example/homework2/snilsValidator/SnilsValidatorImpl.java
+++ src/main/java/org/example/homework2/snilsValidator/SnilsValidatorImpl.java
@@ -3,22 +3,30 @@ package org.example.homework2.snilsValidator;
 public class SnilsValidatorImpl implements SnilsValidator {
     @Override
     public boolean validate(String strSnils) {
-        int controlSumm = Integer.parseInt(strSnils.substring(strSnils.length() - 2));
-        String num = strSnils.substring(0, strSnils.length() - 2).replaceAll("\\D", "");
-
-        int checkNum = 0;
-        for (int i = 0, j = 9; i < num.length(); i++, j--) {
-            checkNum += ((num.charAt(i) - '0') * j);
+        String validateString = strSnils.trim().replaceAll(" ", "");
+        if (verifyStrSnils(validateString)) {
+            int controlSumm = Integer.parseInt(validateString.substring(validateString.length() - 2));
+            String num = validateString.substring(0, validateString.length() - 2).replaceAll("\\D", "");
+            int checkNum = 0;
+            for (int i = 0, j = 9; i < num.length(); i++, j--) {
+                checkNum += ((num.charAt(i) - '0') * j);
+            }
+            if (checkNum > 100) {
+                checkNum %= 101;
+            }
+            if (checkNum == 100) {
+                checkNum = 0;
+            }
+            return checkNum == controlSumm;
         }
+        return false;
+    }
 
-        if (checkNum > 100) {
-            checkNum %= 101;
-        }
-
-        if (checkNum == 100) {
-            checkNum = 0;
-        }
-
-        return checkNum == controlSumm;
+    private boolean verifyStrSnils(String strVal) {
+        if (strVal == null || strVal.equals("") || strVal.length() != 11)
+            return false;
+        if (!strVal.matches("[0-9]*"))
+            return false;
+        return true;
     }
 }
