436eae6745b5e95474175e98af2a2f79e4701cc5
ChinenOFF <pro100cvv@gmail.com>
2023-03-21 17:45:39 +0300

    task #4 PasswordValidator completed!

---
+++ src/main/java/org/example/homework3/password/validator/exception/WrongLoginException.java
@@ -0,0 +1,9 @@
+package org.example.homework3.password.validator.exception;
+
+public class WrongLoginException extends Exception {
+
+  public WrongLoginException(String message) {
+    super(message);
+  }
+
+}
